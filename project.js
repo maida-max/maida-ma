505c53318d608e7fb732395de5e6be3dfda385da
maida-max <maaido203@gmail.com>
2026-02-02 12:39:49 -0800

    Add files via upload

---
+++ project.js
@@ -0,0 +1,41 @@
+// DOM Elements
+const equipmentName = document.getElementById("equipmentName");
+const equipmentType = document.getElementById("equipmentType");
+const equipmentStatus = document.getElementById("equipmentStatus");
+const checkDate = document.getElementById("checkDate");
+const addBtn = document.getElementById("addBtn");
+const equipmentList = document.getElementById("equipmentList");
+const errorMsg = document.getElementById("errorMsg");
+
+// Event Listener
+addBtn.addEventListener("click", function () {
+
+    // Validation
+    if (
+        equipmentName.value === "" ||
+        equipmentType.value === "" ||
+        equipmentStatus.value === "" ||
+        checkDate.value === ""
+    ) {
+        errorMsg.textContent = "Fadlan dhammaan xogta buuxi!";
+        return;
+    }
+
+    errorMsg.textContent = "";
+
+    // Create list item (DOM)
+    const li = document.createElement("li");
+    li.textContent =
+        "Equipment: " + equipmentName.value +
+        " | Type: " + equipmentType.value +
+        " | Status: " + equipmentStatus.value +
+        " | Date: " + checkDate.value;
+
+    equipmentList.appendChild(li);
+
+    // Clear form
+    equipmentName.value = "";
+    equipmentType.value = "";
+    equipmentStatus.value = "";
+    checkDate.value = "";
+});
